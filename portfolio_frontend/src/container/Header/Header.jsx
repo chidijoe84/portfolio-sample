fed5d0eeee95a2f4113f2a8e7a0954e2126c6c19
chidijoe84 <anigbo.joel@gmail.com>
2022-10-05 14:19:37 +0100

    change logo

--- portfolio_frontend/src/container/Header/Header.jsx
+++ portfolio_frontend/src/container/Header/Header.jsx
@@ -30,14 +30,14 @@ const Header = () => {
               <p className='p-text'>
                 Hello, I am
               </p>
-              <h1 className='head-text'>Micael</h1>
+              <h1 className='head-text'>Maxxicov</h1>
 
             </div>
 
           </div>
 
           <div className="tag-cmp app__flex">
-          <p className='p-text'>Web Developer</p>
+          <p className='p-text'>Front Developer</p>
           <p className='p-text'>Freelancer</p>
           </div>
         </div>
@@ -63,7 +63,7 @@ const Header = () => {
       whileInView={scaleVariants.whileInView}
       className='app__header-circles'
       >
-        {[images.flutter, images.redux, images.sass].map((circle,index)=>(
+        {[images.react, images.redux, images.sass].map((circle,index)=>(
           <div className='circle-cmp app__flex' key={`circle-${index}`} >
             <img src={circle} alt="circle" />
           </div>
